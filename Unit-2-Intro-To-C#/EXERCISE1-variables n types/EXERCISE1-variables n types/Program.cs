b8dfaa44290fa125af5d7b839e3dfc06427f874b
Hybrid Theory <jaysinmyers1@gmail.com>
2025-04-07 18:55:42 -0400

    Monday

---
+++ Unit-2-Intro-To-C#/EXERCISE1-variables n types/EXERCISE1-variables n types/Program.cs
@@ -0,0 +1,140 @@
+﻿namespace EXERCISE1_variables_n_types;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        // *****EXERCISE 1.1*********
+        
+        /*Console.WriteLine("Please enter a sentence:  ");  // Ask user to type sentence
+        string theResponse;      // String to hold the line of input from the keyboard
+        theResponse = Console.ReadLine();    // Get the line from the kayboard
+        Console.WriteLine(theResponse);//  repeat the line from user's keyboard  
+*/
+
+        
+        
+        //*******EXERCISE 1.2**********
+        
+       /* int number1 = 0;  //places to hold numbers
+        int plusOne = 1;
+        int sum;
+            
+            
+        Console.WriteLine("Please enter a number to add the number 1 to:  ");  //ask user to type a number
+        string theNumber;  //place to hold the user input
+        theNumber = Console.ReadLine();  //ask user to type the number
+        number1 = int.Parse(theNumber);  //turn string into a integer
+        
+        sum = number1 + plusOne;  //the formula / adding the numbers together
+        
+        Console.WriteLine("The sum of the numbers is: " + sum);  // give the answer
+        */
+       
+       
+       // ****** EXERCISE 1.3 **********
+       
+       /*
+       double a = 0;
+       double point5 = 0.5;
+       double sum;
+       
+       Console.WriteLine("Enter a number to add 0.5 to : ");
+       string myNumber;
+       myNumber = Console.ReadLine();
+       a = double.Parse(myNumber);
+       
+       sum = a + point5;
+       Console.WriteLine(sum);
+
+                        */
+
+
+                //********EXERCICE 1.4 ***********
+                
+          /*      
+                
+       double numeroUno = 0;  //make a place to store the numbers
+       double numeroDos = 0;
+       double sum;   //make a place to store the sum
+       
+       Console.WriteLine("Enter your first number: ");  //ask for the first number
+       numeroUno = double.Parse(Console.ReadLine());  //you have to make it so that the string you input is 
+                                                        //turned into a double
+                                                        
+       Console.WriteLine("Enter your second number: ");  //ask for the second number
+       numeroDos = double.Parse(Console.ReadLine());  //turn string into a double
+       
+       sum = numeroUno + numeroDos;  //create the equation
+       
+       Console.WriteLine($"The sum is: {sum}");  //return the sum
+       
+            */
+
+
+          
+                  //**********EXERCISE 1.5************
+                  
+                  /*
+          
+          double multipleUno = 0;
+          double multipleDos = 0;
+          double sum = 0;
+          
+          Console.WriteLine("enter the first number: ");
+          multipleUno = double.Parse(Console.ReadLine());
+          
+          Console.WriteLine("enter the second number: ");
+          multipleDos = double.Parse(Console.ReadLine());
+          
+          sum = multipleUno * multipleDos;
+          Console.WriteLine($"The sum is: {sum}");
+          
+          */
+
+                  
+                  
+                  //*******EXERCISE 1.6*********
+
+                  /*
+                  
+                  int firstDivision = 0;
+                  int secondDivision = 0;
+                  int sum = 0;
+                  
+                  Console.WriteLine("Enter first number: ");
+                  firstDivision = int.Parse(Console.ReadLine());
+                  
+                  Console.WriteLine("Enter second number: ");
+                  secondDivision = int.Parse(Console.ReadLine());
+                  
+                  sum = firstDivision / secondDivision;
+                  Console.WriteLine($"The sum is: {sum}");
+                  
+                  //When dividing numbers that will have a remainder, the machine will just give you the whole #
+                  
+                  */
+                  
+                  
+                  //*******EXERCISE 1.7**********
+                  
+        string theTruth = "true";
+        string theFalse = "false";
+        string myBoolean;
+        
+        Console.WriteLine("Enter either 'true' or 'false': ");
+        myBoolean = Console.ReadLine();
+
+        if (myBoolean == theTruth)
+        {
+            Console.WriteLine("false");
+        } 
+        if (myBoolean == theFalse)
+
+        {
+           Console.WriteLine("true"); 
+        }
+                  
+    }
+}
--- Unit-2-Intro-To-C#/StringExamples-Frank-V1/StringExamples/Program.cs
+++ Unit-2-Intro-To-C#/StringExamples-Frank-V1/StringExamples/Program.cs
@@ -15,9 +15,11 @@ class Program
          *        you can't use < > to compare strings
          *        Beware: Early versions of C# do not allow use of == with strings
          *
+         * a method() is a program that processes data in an object/variable
+         *
          * C# provides several methods to operate on strings:
          *
-         *    Hey) - compare the string to left of the . to string inside () for equals
+         *     .Equals(string) - compare the string to left of the . to string inside () for equals
          *     .CompareTo(string) - return a number indicating how the first string relates to the second
          *                          return a negative number if first string is less than the second
          *                          return a zero if first string is equal the second
@@ -59,5 +61,46 @@ class Program
         {
             Console.WriteLine("string1 is NOT equal string2");
         }
+        // ********* SOME METHODS THAT PROCESS STRINGS **********
+        // To see a list of all methods available to an object - just type the objectName.
+        // .Length - returns the number of characters in a string
+        // .Contains(string) - returns true if the string given is inside the string 
+        // .Substring(start-index,length) - extract characters from thw string starting at the start index
+        //                                  for the numbers characters indicated by the length
+        //                                  start-index starts at 0 NOT 1
+        //                                  whenever you see the word index -starts counting at 0
+        
+        // .StartsWith(string) - return True if string starts with the specified characters (CASE SENSITIVE)
+        // .EndsWith(string) - return True if string ends with the specified characters (CASE SENSITIVE)
+        // .Trim() - removes any leading or trailing spaces
+        //
+        
+        //.                01234567891123456789212345678931234567894123
+        string sentence = "   My name is Jaysin and I am learning C#   ";
+        Console.WriteLine("There are " + sentence.Length + " characters in the sentence");
+        
+        bool containsJaysin = sentence.Contains("Jaysin"); //True if the sentence contains Jaysin
+        Console.WriteLine("Does sentence contain Jaysin: " + containsJaysin);
+        
+        Console.WriteLine("Does sentence contain Olive: " + sentence.Contains("Olive"));
+
+        string nameInSentence = sentence.Substring(11, 6); // Extract the 6 chars starting at position 12
+        Console.WriteLine("The name is: " + nameInSentence);
+        
+        Console.WriteLine("Results when sentence.Trim() is used to reomove leading and trailing spaces: " + sentence);
+        Console.WriteLine("Does the sentence start with My? " + sentence.Trim().StartsWith("My"));
+        Console.WriteLine("Does the sentence start with my? " + sentence.Trim().StartsWith("my"));
+        Console.WriteLine("Does the sentence end with C#? " + sentence.Trim().EndsWith("C#"));
+        
+        // When C# sees a statement with chained-operations (operations seperated by .)
+        // C# Process the statement from left to right, one operation at a time
+        // Then, it uses the result of the operation in the next one
+        //
+        // sentence.Trim().EndsWith("C#")
+        //
+        // 1.   sentence.Trim().EndsWith("C#") - returns a string with the leading and trailing spaces removed
+        // 2.   results-from-step-1.EndsWith() - return a true or false depending on if the string actuallu ends with C#
+        //
+
     } // End of Main()
 }  // End of class Program
